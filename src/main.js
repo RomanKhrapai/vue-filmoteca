a0c7afc3db9c3ba5402b50bd91d7e8c3a621e711
RomanKhrapai <hrapayr@gmail.com>
2023-10-16 11:48:24 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -8,8 +8,6 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
-import CustomButton from "./components/CustomButton.vue";
-
 const vuetify = createVuetify({ components, directives });
 
 const app = createApp(App);
