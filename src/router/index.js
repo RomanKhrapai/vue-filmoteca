49ea92deefbbca32f45d76f7c9c998eaf1d0627e
RomanKhrapai <hrapayr@gmail.com>
2023-10-19 11:00:10 +0300

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ import User from "../components/UserPage.vue";
 
 const routes = [
     {
-        path: "/hw_vue_5",
+        path: "/hw_vue_5/",
         name: "home",
         component: Home,
         meta: { id: 1 },
